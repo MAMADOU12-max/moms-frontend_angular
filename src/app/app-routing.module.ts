1068aebf5573fc7c7e034aea537a3c77c2897c30
Mamadou Diouf <modoudiouf535@gmail.com>
2022-01-08 17:33:34 +0000

    front-end MOMS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginPageComponent},
+  {
+    path: 'moms',
+    component: LayoutComponent,
+    children: [{
+        path:'',
+        loadChildren: () => import('./layout/layout-routing.module').then(m => m.LayoutRoutingModule)
+    }]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MachineComponent } from './machine/machine.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MachineComponent,
+    DashboardComponent,
+    SettingsComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
